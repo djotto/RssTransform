b6783a4afffce5c4e4f6b43eda8a509e2f998121
David Otton <david@otton.org>
2024-01-21 11:09:17 +0000

    Closes #5 Load config.yml from config directory, else error

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/djotto/rss-transform/config"
+	_ "net/http/pprof" //nolint:gosec
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	if err := config.Init(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s: %+v\n", config.Dir, config.AppConfig)
 }
